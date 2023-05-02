3e9eb7843c88e4ae5095d8070acf6fa5ef1907da
alguiennn07 <130928761+alguiennn07@users.noreply.github.com>
2023-05-02 21:44:27 +0200

    Update gen_contranseña.py

--- gen_contranseña.py
+++ gen_contranseña.py
@@ -1,19 +1,38 @@
 
 import string
+
 from random import choices,shuffle,choice
 
-
 while True:
+
     signos=choices(['@' , '#' , "?" , "¿" '¡' , "!" , '*' , '/' , '-' , '_'],k=3)
+
     letras=choices(string.ascii_letters+ string.ascii_uppercase,k=5)
+
     prim_letras=choices(string.ascii_letters + string.ascii_uppercase,k=2)
+
     cuerpo=*signos,*letras
+
     todo=*prim_letras,*cuerpo
+
     input("Presiona enter para generar una contraseña: ")
+
     if input:
+
        at=str(prim_letras),str(cuerpo)
+
        print(*todo,sep="")
+
+
+
     
+    
+    
+   
+    
+    
+
+ 
     
 
 
